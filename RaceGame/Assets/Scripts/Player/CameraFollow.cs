6bbda0d575f23f02abaf4827966f1d7f763d1278
FrozenScorpio <116769282+FrozenScorpio@users.noreply.github.com>
2023-03-20 21:05:42 -0400

    Change the Game Scene and finish the movement (cross fingers)
    and started a camera follow script

---
+++ RaceGame/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 offset;
+    public Transform target;
+    public float translateSpeed;
+    public float rotationSpeed;
+
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        HandleTranslation();
+        HandleRotation();
+    }
+
+    private void HandleTranslation()
+    {
+        var targetPosition = target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+    }
+
+    private void HandleRotation()
+    {
+        var direction = target.position = transform.position;
+        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
+    }
+}
