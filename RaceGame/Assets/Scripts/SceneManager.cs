a0367f1d681a54eff9434d8a229b79f6660fa039
nickab56 <nick.abegg@yahoo.com>
2023-04-01 22:31:13 -0400

    Update SceneManager.cs

--- RaceGame/Assets/Scripts/SceneManager.cs
+++ RaceGame/Assets/Scripts/SceneManager.cs
@@ -13,7 +13,7 @@ public class SceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Teleport();
+        
     }
 
     // Update is called once per frame
