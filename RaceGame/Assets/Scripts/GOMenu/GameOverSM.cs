1d69f56c8ee8a7cb7223e5c2da5f1167f6f98fc2
nickab56 <nick.abegg@yahoo.com>
2023-04-03 13:04:59 -0400

    Implemented timing with splash screen I think

--- RaceGame/Assets/Scripts/GOMenu/GameOverSM.cs
+++ RaceGame/Assets/Scripts/GOMenu/GameOverSM.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameOverSM : MonoBehaviour
 {
     public float delayAmount = 0.75f;
+    public TMP_Text PlayerTimeTxt;
+    public TMP_Text HighTimeTxt;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerTimeTxt.text = "Time   " + Constants.C.timeCount.ToString("0.00");
+        HighTimeTxt.text = "Best Time   " + Constants.C.HighTime.ToString("0.00");
     }
 
     // Update is called once per frame
