89809b4bf80b066f905279de60f4dcc8a07840aa
nickab56 <nick.abegg@yahoo.com>
2023-04-03 23:24:34 -0400

    particles turn on when level changes

--- RaceGame/Assets/Scripts/ChangeSkybox.cs
+++ RaceGame/Assets/Scripts/ChangeSkybox.cs
@@ -6,10 +6,12 @@ public class ChangeSkybox : MonoBehaviour
 {
     public Material NomralSkybox;
     public Material DestroyedSkybox;
+    public GameObject Particles;
     // Start is called before the first frame update
     void Start()
     {
         RenderSettings.skybox = DestroyedSkybox;
+        Particles.SetActive(true);
     }
 
     // Update is called once per frame
