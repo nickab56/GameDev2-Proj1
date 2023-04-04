f8e35d18a03c080552abf521fc0c5ce0e56f1654
nickab56 <nick.abegg@yahoo.com>
2023-04-03 23:49:58 -0400

    level swap effects

--- RaceGame/Assets/Scripts/ChangeSkybox.cs
+++ RaceGame/Assets/Scripts/ChangeSkybox.cs
@@ -7,11 +7,18 @@ public class ChangeSkybox : MonoBehaviour
     public Material NomralSkybox;
     public Material DestroyedSkybox;
     public GameObject Particles;
+    public Light Sun;
+    public AudioSource EpicMusic;
+    public AudioSource FutureMusic;
+
     // Start is called before the first frame update
     void Start()
     {
         RenderSettings.skybox = DestroyedSkybox;
         Particles.SetActive(true);
+        Sun.enabled = false;
+        EpicMusic.Play();
+        FutureMusic.Stop();
     }
 
     // Update is called once per frame
